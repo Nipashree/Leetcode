b2877ec97a65e71abe9aef1b60eb1d862b6a1bf9
Nipashree Bhattacharya <146835960+Nipashree@users.noreply.github.com>
2026-06-21 23:38:36 +0530

    leetcode

--- (**Imp)Divide 2 int.java
+++ (**Imp)Divide 2 int.java
@@ -1,7 +1,7 @@
 class Solution {
     public int divide(int dividend, int divisor) {
         
-        // Overflow case
+        // Overflow 
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
