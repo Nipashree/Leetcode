e84baa596fdcca1a0fbe62f1f269e7e23f20b2ea
Nipashree Bhattacharya <146835960+Nipashree@users.noreply.github.com>
2026-08-16 23:41:07 +0530

    Implement summaryRanges function in Summary Ranges.py

---
+++ Summary Ranges.py
@@ -0,0 +1,14 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        res = []
+        i, n = 0, len(nums)
+        while i < n:
+            start = i
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+            if start == i:
+                res.append(f"{nums[start]}")
+            else:
+                res.append(f"{nums[start]}->{nums[i]}")
+            i += 1
+        return res
